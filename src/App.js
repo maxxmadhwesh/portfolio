f21c42121735e413af968063b03e3a74e27a48f2
m0a0er7 <madhwesh.anand@walmart.com>
2024-08-28 16:51:28 +0530

    Creating and pushing the basic react app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I am Madhwesh Anand
         </a>
       </header>
     </div>
